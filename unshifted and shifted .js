cc9f5a63c0fd857cef71e5990eddf7688129f992
mahesh091298 <59042478+mahesh091298@users.noreply.github.com>
2019-12-19 12:05:54 +0530

    Add files via upload

---
+++ unshifted and shifted .js
@@ -0,0 +1,16 @@
+var ourarray=[["mahesh",3],["pooja",2]];
+undefined
+var removedfromourarray=ourarray.shift();
+undefined
+console.log(ourarray);
+VM1431:1 [Array(2)]0: (2) ["pooja", 2]length: 1__proto__: Array(0)
+undefined
+var ourarray=[["mahesh",3],["pooja",2]];
+undefined
+ourarray.unshift(["manoj",90]);
+3
+console.log(ourarray);
+VM1566:1 (3) [Array(2), Array(2), Array(2)]0: (2) ["manoj", 90]1: (2) ["mahesh", 3]2: (2) ["pooja", 2]length: 3__proto__: Array(0)
+undefined
+function ourreusablefunction((){
+VM1630:1 Uncaught SyntaxError: Unexpected token '('
