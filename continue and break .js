41ad478ee2fceac5bc6bf953a6f5691e02aa928f
mahesh091298 <59042478+mahesh091298@users.noreply.github.com>
2019-12-19 12:24:35 +0530

    Add files via upload

---
+++ continue and break .js
@@ -0,0 +1,27 @@
+for ( var i=0;i<=5;i++)
+    console.log(i);
+VM3207:2 0
+VM3207:2 1
+VM3207:2 2
+VM3207:2 3
+VM3207:2 4
+VM3207:2 5
+undefined
+for ( var i=0;i<=5;i++){
+    if(i==3){
+        console.log("element found");
+        break;
+    }
+}
+VM3390:3 element found
+undefined
+for ( var i=0;i<=5;i++){
+    if(i==3){
+        
+        break;
+    }
+}
+undefined
+console.log(i);
+VM3513:1 3
+undefined
